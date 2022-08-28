e94f2fe500a7503ccd6a0216abf29f0c1d91c71e
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-08-28 22:04:34 +0300

    added img to NotFound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,6 +1,14 @@
 import { Container } from 'react-bootstrap';
+import image from './not-found-img.png';
+import styles from './NotFound.module.css';
+
 export const NotFound = () => (
   <Container className="d-flex flex-column align-items-center my-5">
-    <h2>Page not found (404)</h2>
+    <div>
+      <h2>Page not found (404)</h2>
+    </div>
+    <div>
+      <img src={image} alt="" className={styles.img}></img>
+    </div>
   </Container>
 );
